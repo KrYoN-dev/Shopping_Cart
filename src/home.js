8a2262c0fbda05058365c1142e6e5fec859f1203
kryon22 <shubhamkharabe02@gmail.com>
2022-01-28 10:53:56 +0530

    First Commit

---
+++ src/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProductList from './productList';
+
+const Home = () =>{
+    return(
+        <>
+            <h1 className='text-center mt-5 pt-5'> Continue Shopping </h1>
+            <ProductList/>
+        </>
+    )
+}
+
+export default Home;
